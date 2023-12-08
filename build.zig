45690da64f4936a12c72954141f04bcb9b72833f
capthehacker99 <pawat.capsule@gmail.com>
2023-12-08 14:43:09 +0700

    Updated to use GitRepoStep.zig

--- build.zig
+++ build.zig
@@ -1,8 +1,17 @@
 const std = @import("std");
-
+const GitRepoStep = @import("GitRepoStep.zig");
 
 pub fn build(b: *std.Build) void {
-    const zigwin32 = b.dependency("zigwin32", .{});
+    const zigwin32_repo = GitRepoStep.create(b, .{
+        .url = "https://github.com/marlersoft/zigwin32",
+        .branch = "main",
+        .sha = "6777f1db221d0cb50322842f558f03e3c3a4099f",
+    });
+    const zigwin32 = b.createModule(.{
+        .source_file = .{
+            .path = b.pathJoin(&.{ zigwin32_repo.path, "win32.zig" }),
+        }
+    });
     const win2sys = b.addModule("win2sys", .{
         .source_file = .{
             .path = "src/win2sys.zig",
@@ -10,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .dependencies = &.{
             .{
                 .name = "win32",
-                .module = zigwin32.module("zigwin32")
+                .module = zigwin32
             }
         }
     });
@@ -19,7 +28,8 @@ pub fn build(b: *std.Build) void {
             .path = "src/win2sys.zig"
         },
     });
-    tests.addModule("win32", zigwin32.module("zigwin32"));
+    tests.step.dependOn(&zigwin32_repo.step);
+    tests.addModule("win32", zigwin32);
     b.step("test", "Run library tests.").dependOn(&tests.step);
     const exe = b.addExecutable(.{
         .name = "Win2Sys Su",
@@ -27,7 +37,8 @@ pub fn build(b: *std.Build) void {
             .path = "example/su.zig"
         },
     });
-    exe.addModule("win32", zigwin32.module("zigwin32"));
+    exe.step.dependOn(&zigwin32_repo.step);
+    exe.addModule("win32", zigwin32);
     exe.addModule("win2sys", win2sys);
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
