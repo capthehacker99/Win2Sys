d168f0ce020fd534c5cdb5a87346de39c5b8235d
capthehacker99 <pawat.capsule@gmail.com>
2023-12-17 10:00:39 +0700

    Very gay ik.

--- build.zig
+++ build.zig
@@ -1,17 +1,23 @@
 const std = @import("std");
 const GitRepoStep = @import("GitRepoStep.zig");
 
-pub fn build(b: *std.Build) void {
-    const zigwin32_repo = GitRepoStep.create(b, .{
-        .url = "https://github.com/marlersoft/zigwin32",
-        .branch = "main",
-        .sha = "6777f1db221d0cb50322842f558f03e3c3a4099f",
-    });
-    const zigwin32 = b.modules.get("zigwin32") orelse b.addModule("zigwin32", .{
-        .source_file = .{
-            .path = b.pathJoin(&.{ zigwin32_repo.path, "win32.zig" }),
-        }
-    });
+pub fn build(b: *std.Build) !void {
+    const zigwin32 = blk: {
+        break :blk b.modules.get("zigwin32") orelse {
+            const zigwin32_repo = GitRepoStep.create(b, .{
+                .url = "https://github.com/marlersoft/zigwin32",
+                .branch = "main",
+                .sha = "6777f1db221d0cb50322842f558f03e3c3a4099f",
+            });
+            var prog_node: std.Progress.Node = undefined;
+            try zigwin32_repo.step.make(&prog_node);
+            break :blk b.addModule("zigwin32", .{
+                .source_file = .{
+                    .path = b.pathJoin(&.{ zigwin32_repo.path, "win32.zig" }),
+                }
+            });
+        };
+    };
     const win2sys = b.addModule("win2sys", .{
         .source_file = .{
             .path = "src/win2sys.zig",
@@ -28,7 +34,6 @@ pub fn build(b: *std.Build) void {
             .path = "src/win2sys.zig"
         },
     });
-    tests.step.dependOn(&zigwin32_repo.step);
     tests.addModule("win32", zigwin32);
     b.step("test", "Run library tests.").dependOn(&tests.step);
     const exe = b.addExecutable(.{
@@ -37,7 +42,6 @@ pub fn build(b: *std.Build) void {
             .path = "example/su.zig"
         },
     });
-    exe.step.dependOn(&zigwin32_repo.step);
     exe.addModule("win32", zigwin32);
     exe.addModule("win2sys", win2sys);
     const run_cmd = b.addRunArtifact(exe);
